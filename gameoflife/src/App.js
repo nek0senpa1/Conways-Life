fdb68c233f3521676215c169594a90f0ab4fbc6b
amberm <nekosenpai@gmail.com>
2019-07-19 00:35:44 -0600

    making a mystery... of life

---
+++ gameoflife/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Conway's Game of Life
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
